c4f9891866de7f69451af50b0f0171f93e7fc696
LorikDullovi <lorikdullovi@gmail.com>
2024-01-07 20:46:06 +0100

    Faqja e pare

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> AppSchool </title>
+    <link rel="stylesheet" href="indexx.css">
+</head>
+
+<body>
+    <header  class="header" style="background-color: #F5F5DC;">
+        <div id="position" class="container">
+            <div class="logopage">
+                <img class="logoimg" src="photo/Appschool.png">
+            </div>
+            <div class="table">
+                <ul class="headerul" style="background-color: none">
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="#contact">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="account">
+                <a class="loginlink" href="login.php"><button class="loginbutton"  style="background-color:#6B8E23;border:1px;border-radius:7px;margin-right:10px">Login</button></a>
+                <a class="signuplink" href="signup.php">
+                    <button class="signupbutton"  style="background-color:#6B8E23"  >Sign-up</button></a>
+            </div>
+        </div>
+    </header>
+    <main id="Home" style="background-color:6B8E23" class="menumain" >
+        <div class="menudiv" >
+            <div class="seconddiv">
+                <h1 class="menuh1" style="color:black">The School Page </h1>
+                <p class="menup" style="color:black">A school is an educational institution designed to provide learning 
+                    spaces and learning environments for the teaching of students under the direction 
+                    of teachers. Most countries have systems of formal education, which is sometimes compulsory.<br></p>
+                <div class="presentationdiv"> <a href="https://www.youtube.com/watch?v=WlWKru6KgWE" target="blank">
+
+                        <div class="playbutton" style="color:black">
+
+                        </div>
+                    </a>
+                </div>
+                <p class="presentationp" style="color:black"><u> Watch the video on youtube
+                </u></p>
+            </div>
+           <div class="bgimgdiv">
+                <img class="bgimg" src="photo/Schools.jpg">
+            </div>
+        </div>
+        <footer class="footer" style="background-color: #F5F5DC;">
+            <div id="contact" class="contactform">
+            <div class="infoo">
+                <h2>Contact</h2>
+                </div>
+                
+                <div class="info">
+                    <h2 class="infoh2">How to find us</h2>
+                    <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d99370.14183977399!2d-77.08461583521711!3d38.89386718939999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7c6de5af6e45b%3A0xc2524522d4885d2a!2sWashington%2C%20Distrikti%20i%20Kolumbis%C3%AB%2C%20Shtetet%20e%20Bashkuara%20t%C3%AB%20Amerik%C3%ABs!5e0!3m2!1ssq!2s!4v1674827908926!5m2!1ssq!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <h3>Head office</h3>
+                    <div class="location">
+                        <div class="d1">
+                            <img class="streetimg" src="photo/street.jpg">
+                        </div>
+                        <div class="d2">
+                            <p class="streetp">10 Washington, USA, E1 1EC</p>
+                        </div>
+                    </div>
+                    <div class="inboxdiv">
+                        <div class="divimg">
+                            <img class="inboximg" src="photo/inbox.jpg">
+                        </div>
+                        <div class="emailtext">
+                            <a class="linkemail" href="mailto:lr52949@ubt-uni.net">washington@gmail.com</a>
+                        </div>
+                    </div>
+                    <div class="pnumberdiv">
+                        <div class="phoneimgdiv">
+                            <img class="phoneimg" src="photo/phone.jpg">
+                        </div>
+                        <div class="phonenumber">
+                            <p>+383 43 975 810</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="socialm">
+                <div class="design">
+                    <p class="designp">© 2022 SchoolApp. Designed by<a class="designlink"
+                            href="https://themeforest.net/user/epic-themes/portfolio?irgwc=1&clickid=3wCw0WwQUxyIUNGVPPU0LViWUkG31STUPxSp1o0&iradid=275988&irpid=419114&iradtype=ONLINE_TRACKING_LINK&irmptype=mediapartner&mp_value1=&utm_campaign=af_impact_radius_419114&utm_medium=affiliate&utm_source=impact_radius"
+                            target="_blank">Epic-Themes</a></p>
+                </div>
+                <div class="medias">
+                    <p class="links">Follow us: </p>
+                    <a href="https://www.instagram.com/" target="_blank"><img id="logos" class="instagram"
+                            src="photo/instagramm.png"></a>
+                    <a href="https://www.facebook.com/" target="_blank"><img id="logos" class="facebook"
+                            src="photo/fb.jpg"></a>
+                    <a href="https://myaccount.google.com/?utm_source=account-marketing-page&utm_medium=go-to-account-button&pli=1"
+                        target="_blank"><img id="logos" class="google" src="photo/google.jpg"></a>
+                    <a href="https://github.com/" target="_blank"><img id="logos" class="github"
+                            src="photo/github.jpg"></a>
+                </div>
+            </div>
+        </footer>
+    </main>
+
+</body>
+
+</html>
